7e0e813e9bc9d36ddb3f3393712f3d225fd70a55
Ashwin <ash6chin8@gmail.com>
2021-06-10 14:24:21 +0530

    feature upgrade

--- app.js
+++ app.js
@@ -76,15 +76,16 @@ app.get("/manualtrg/auth/" + process.env.AUTH_KEY, (req, res) => {
 });
 
 
-app.get("/autotrg/ifttt/auth/" + process.env.AUTH_KEY, (req, res) => {
+app.get("/autotrg/ifttt/auth/" + process.env.AUTH_KEY2, (req, res) => {
   res.send("Action Started");
   const params = {
     api_token: process.env.API_KEY
   }
   var websiteContent;
-  var fiftyQuotes;
-  var todaysQuote;
+  var quote;
+  var onTodaysDay;
 
+  //buzzsprout
   axios.get(" https://www.buzzsprout.com/api/1173590/episodes.json", {
       params
     })
@@ -95,10 +96,26 @@ app.get("/autotrg/ifttt/auth/" + process.env.AUTH_KEY, (req, res) => {
       console.log(error);
     });
 
+
+  //on todays days
   function postfech01() {
-    axios.get("https://zenquotes.io/api/today/", )
+    var today = new Date();
+    // using date offset as heroku is hosted in usa
+    var dd = String(parseInt(String(today.getDate() + parseInt(process.env.DATE_OFFSET))));
+    var mm = String(parseInt(String(today.getMonth() + 1)));
+    axios.get("https://history.muffinlabs.com/date/" + mm + "/" + dd, )
       .then(response => {
-        todaysQuote = response.data[0].h;
+        var a = 1;
+        response.data.data.Events.forEach((item, i) => {
+          if (!(item.html.toUpperCase().includes("WAR") || item.html.toUpperCase().includes("MURDER") || item.html.toUpperCase().includes("BATTLE") || item.html.toUpperCase().includes("ARMY"))) {
+            a = a + 1;
+            if (typeof onTodaysDay == "undefined") {
+              onTodaysDay = "1) in year " + item.html + "<br/><br/>";
+            } else {
+              onTodaysDay = onTodaysDay + a + ") in year " + item.html + "<br/><br/>";
+            }
+          }
+        });
         postfech02();
       }).catch(error => {
         console.log(error);
@@ -107,24 +124,18 @@ app.get("/autotrg/ifttt/auth/" + process.env.AUTH_KEY, (req, res) => {
 
 
   function postfech02() {
-    axios.get("https://zenquotes.io/api/quotes/", )
+    axios.get("https://zenquotes.io/api/random", )
       .then(response => {
-        const websiteContent = response.data;
-        websiteContent.forEach((item, i) => {
-          if (typeof fiftyQuotes == "undefined") {
-            fiftyQuotes = 1 + item.h + "<br />";
-          } else {
-            fiftyQuotes = fiftyQuotes + (i + 1) + item.h + "<br />";
-          }
-        })
+        quote = response.data[0].h;
         postfech03();
+
       }).catch(error => {
         console.log(error);
       });
   }
 
   function postfech03() {
-    longStringOfInformation = "<!DOCTYPE html><html><head></head><body> <h1>Quotes And Stats</h1> <h2>On todays day<h2/><br/><h3>" + todaysQuote + "</h3> <h2>Quotes / ideas for your stories</h2> <br /> <p>" + fiftyQuotes + "</p> <br /> <h2>Stats</h2><h4>Total entries " + websiteContent.length + "</h4> <p>";
+    longStringOfInformation = "<!DOCTYPE html><html><head></head><body> <h1>On this day, Quote And Stats</h1> <h2>On todays day<h2/><br/><strong><h6>" + onTodaysDay + "</h6></strong> <h2>Quote</h2> <br /> <strong><h3>" + quote + "<h3></strong> <br /> <h2>Stats</h2><h4>Total entries " + websiteContent.length + "</h4> <p>";
     // sort data based on highest value
     websiteContent.sort((a, b) => {
       if (a.total_plays > b.total_plays) {
@@ -141,10 +152,10 @@ app.get("/autotrg/ifttt/auth/" + process.env.AUTH_KEY, (req, res) => {
     const arrayLength = websiteContent.length;
     websiteContent.forEach((item, i) => {
       total_plays = total_plays + item.total_plays;
-      longStringOfInformation = longStringOfInformation + "Title: " + item.title + " ;  <a href=" + item.audio_url.toString() + "> Location</a> " + " Length (in seconds): " + item.duration + ";  No of plays (Worldwide): " + item.total_plays + "; Published date: " + item.published_at + ";<br/><br/>";
+      longStringOfInformation = longStringOfInformation + "Title: " + item.title + " ;  <a href=" + item.audio_url.toString() + "> Location</a> " + " Length (HH:MM:SS): " + new Date(item.duration * 1000).toISOString().substr(11, 8) + ";  No of plays (Worldwide): " + item.total_plays + "; Published date: " + item.published_at.split("T")[0] + ";<br/><br/>";
     });
 
-    longStringOfInformation = longStringOfInformation + "</p><h3><strong>Total plays on all podcasts " + total_plays + "</strong><h3></body></html>"
+    longStringOfInformation = longStringOfInformation + "</p><h3><strong>Total plays on all podcasts " + total_plays + "</strong><h3></body></html>";
 
     var mail = nodemailer.createTransport({
       service: "gmail",
@@ -157,7 +168,7 @@ app.get("/autotrg/ifttt/auth/" + process.env.AUTH_KEY, (req, res) => {
     var mailOptions = {
       from: "demotestoauth@gmail.com",
       to: process.env.TARGET_MAIL_ID,
-      subject: "Automatic Podcast Progress Update From Ashwin's Code",
+      subject: "Automatic Podcast Progress Update, On This Day And A Quote From Ashwin's Code",
       html: longStringOfInformation
     };
 
