a0bd722df4d50e310bb502948e64996a81a9c01e
Ashwin <ash6chin8@gmail.com>
2021-08-28 21:06:22 +0530

    many many mehdi bugs remanining

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const axios = require("axios");
 const nodemailer = require("nodemailer");
 const app = express();
 
+var autorename;
+var rename = [];
+var replacementbit = false;
+var authors = [];
+
 //body
 
 var longStringOfInformation;
@@ -54,8 +59,11 @@ app.get("/autotrg/ifttt/auth/" + process.env.AUTH_KEY2, (req, res) => {
     });
     const highestValue = websiteContent[1].total_plays;
     const lowestValue = websiteContent[(websiteContent.length - 1)].total_plays;
-    const fiveUnEqualDivisions = ((highestValue - lowestValue)/5);
-    const first = (fiveUnEqualDivisions - 50), second = (fiveUnEqualDivisions * 2 - 50), third = (fiveUnEqualDivisions * 3 - 50), forth = (fiveUnEqualDivisions * 4 - 50);
+    const fiveUnEqualDivisions = ((highestValue - lowestValue) / 5);
+    const first = (fiveUnEqualDivisions - 50),
+      second = (fiveUnEqualDivisions * 2 - 50),
+      third = (fiveUnEqualDivisions * 3 - 50),
+      forth = (fiveUnEqualDivisions * 4 - 50);
     //filter data
     var htmlString;
     var total_plays = 0;
@@ -81,31 +89,139 @@ app.get("/autotrg/ifttt/auth/" + process.env.AUTH_KEY2, (req, res) => {
       total_plays = total_plays + item.total_plays;
     });
 
-    longStringOfInformation = longStringOfInformation + "</p><h3><strong>Total plays on all podcasts " + total_plays + "</strong><h3>" + htmlString +"</body></html>";
-
-    var mail = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "demotestoauth@gmail.com",
-        pass: process.env.DEMO_ACCOUNT_PASSWORD
-      }
-    });
-
-    var mailOptions = {
-      from: "demotestoauth@gmail.com",
-      to: process.env.TARGET_MAIL_ID,
-      subject: "Automatic Podcast Progress Update And A Quote From Ashwin's Code",
-      html: longStringOfInformation
-    };
-
-    mail.sendMail(mailOptions, function(error, info) {
-      if (error) {
-        console.log(error);
-      }
-    });
-
+    longStringOfInformation = longStringOfInformation + "</p><h3><strong>Total plays on all podcasts " + total_plays + "</strong><h3>" + htmlString + "</body></html>";
+    email(longStringOfInformation);
   }
 });
+
+
+function email(html) {
+  var mail = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "demotestoauth@gmail.com",
+      pass: process.env.DEMO_ACCOUNT_PASSWORD
+    }
+  });
+
+  var mailOptions = {
+    from: "demotestoauth@gmail.com",
+    to: process.env.TARGET_MAIL_ID,
+    subject: "Automatic Podcast Progress Update And A Quote From Ashwin's Code",
+    html: html
+  };
+
+  mail.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    }
+  });
+}
+
+
+app.get("/smartrename", (req, res) => {
+  const params = {
+    api_token: process.env.API_KEY
+  };
+  axios.get("https://www.buzzsprout.com/api/1173590/episodes.json", {
+    params
+  }).then((response) => {
+    websiteContent = response.data;
+    autoren();
+    // rename.forEach((item, i) => {
+    //   put
+    // });
+
+  }).catch((error) => {
+    res.send(error);
+  });
+});
+
+function put(id, name) {
+  axios.put("https://www.buzzsprout.com/api/1173590/episodes/" + id.toString() + ".json", {
+    "title": name
+  }, {
+    headers: {
+      "Authorization": "Token token=" + process.env.API_KEY,
+      "Content-Type": "application/json"
+    }
+  });
+}
+
+function autoren() {
+  websiteContent.forEach((item, i) => {
+    if (typeof item.title.split("(")[1] == 'undefined') {
+      autorename = autorename + '<strong><H1 "style=color: red;"> ❌ This episode has no author! please rename is as soon as possible. ❌</H1><br/><h6>The title is ' + item.title + '</h6></strong>';
+    } else {
+      console.log("0");
+      var author = TextCleaner(item.title.split("(")[1]).remove(')').trim().valueOf();
+      if (typeof authors.find(element => element == author) == 'undefined') {
+        console.log("1");
+        replacementbit = true;
+        if (author.toLowerCase().search("&%anuradha%&")) {
+          rename.push({
+            "id": item.id,
+            "title": item.title.split("(")[0] + "( प्रा.सौ अनुराधा भडसावळे. )"
+          });
+          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( प्रा.सौ अनुराधा भडसावळे. )";
+        } else if (author.toLowerCase().search("&%shaila%&") !== -1) {
+          rename.push({
+            "id": item.id,
+            "title": item.title.split("(")[0] + "( प्रप्रा. सौ शैला काळकर. )"
+          });
+          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( प्रप्रा. सौ शैला काळकर. )";
+        } else if (author.toLowerCase().search("&%rajan%&") !== -1) {
+          rename.push({
+            "id": item.id,
+            "title": item.title.split("(")[0] + "( सौ. रंजना जितेंद्र पाटील. )"
+          });
+          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( सौ. रंजना जितेंद्र पाटील. )";
+        } else if (author.toLowerCase().search("&%mona%&") !== -1) {
+          rename.push({
+            "id": item.id,
+            "title": item.title.split("(")[0] + "( श्रीमती मृदुला श्रीनिवास गडनीस. )"
+          });
+          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( श्रीमती मृदुला श्रीनिवास गडनीस. )";
+        } else if (author.toLowerCase().search("&%madhavi%&") !== -1) {
+          rename.push({
+            "id": item.id,
+            "title": item.title.split("(")[0] + "( डॉ. माधवी ठाकूरदेसाई. )"
+          });
+          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( डॉ. माधवी ठाकूरदेसाई. )";
+        } else if (author.toLowerCase().search("&%asavari%&") !== -1) {
+          rename.push({
+            "id": item.id,
+            "title": item.title.split("(")[0] + "( सौ आसावरी काळे. )"
+          });
+          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( सौ आसावरी काळे. )";
+        } else if (author.toLowerCase().search("&%manjiri%&") !== -1) {
+          rename.push({
+            "id": item.id,
+            "title": item.title.split("(")[0] + "( सौ मंजिरी फाटक. )"
+          });
+          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( सौ मंजिरी फाटक. )";
+        } else if (author.toLowerCase().search("&%kapil%&") !== -1) {
+          rename.push({
+            "id": item.id,
+            "title": item.title.split("(")[0] + "( श्री. कपिल देशपांडे. )"
+          });
+          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( श्री. कपिल देशपांडे. )";
+        } else if (author.toLowerCase().search("&%sandhya%&") !== -1) {
+          rename.push({
+            "id": item.id,
+            "title": item.title.split("(")[0] + "( श्रीमती संध्या देवस्थळे. )"
+          });
+          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( श्रीमती संध्या देवस्थळे. )";
+        } else {
+          autorename = autorename + '<strong><H1 "style=color: red;">❌fatal error occured ' + author + " not found </H1><h6>title " + item.title + "<H6></strong>";
+          console.log(item.title);
+        }
+        console.log(rename)
+      }
+    }
+  });
+
+}
 
 var port = process.env.PORT || 3000;
 
