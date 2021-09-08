e6c00a7c4b92c8cd1263392312e49df9bdd0bc93
Ashwin <ash6chin8@gmail.com>
2021-09-08 11:22:33 +0530

    Revert "fixed biasing (even hatefuller commit)"
    This reverts commit 3f193d8344615ae189b4cc958aab1cb6f567425a.

--- app.js
+++ app.js
@@ -5,11 +5,6 @@ const axios = require("axios");
 const nodemailer = require("nodemailer");
 const app = express();
 
-var autorename;
-var rename = [];
-var replacementbit = false;
-var authors = [];
-
 //body
 
 var longStringOfInformation;
@@ -116,111 +111,6 @@ function email(html) {
       console.log(error);
     }
   });
-}
-
-
-app.get("/smartrename", (req, res) => {
-  const params = {
-    api_token: process.env.API_KEY
-  };
-  axios.get("https://www.buzzsprout.com/api/1173590/episodes.json", {
-    params
-  }).then((response) => {
-    websiteContent = response.data;
-    autoren();
-    // rename.forEach((item, i) => {
-    //   put
-    // });
-
-  }).catch((error) => {
-    res.send(error);
-  });
-});
-
-function put(id, name) {
-  axios.put("https://www.buzzsprout.com/api/1173590/episodes/" + id.toString() + ".json", {
-    "title": name
-  }, {
-    headers: {
-      "Authorization": "Token token=" + process.env.API_KEY,
-      "Content-Type": "application/json"
-    }
-  });
-}
-
-function autoren() {
-  websiteContent.forEach((item, i) => {
-    if (typeof item.title.split("(")[1] == 'undefined') {
-      autorename = autorename + '<strong><H1 "style=color: red;"> ❌ This episode has no author! please rename is as soon as possible. ❌</H1><br/><h6>The title is ' + item.title + '</h6></strong>';
-    } else {
-      console.log("0");
-      var author = TextCleaner(item.title.split("(")[1]).remove(')').trim().valueOf();
-      if (typeof authors.find(element => element == author) == 'undefined') {
-        console.log("1");
-        replacementbit = true;
-        if (author.toLowerCase().search("&%anuradha%&")) {
-          rename.push({
-            "id": item.id,
-            "title": item.title.split("(")[0] + "( प्रा.सौ अनुराधा भडसावळे. )"
-          });
-          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( प्रा.सौ अनुराधा भडसावळे. )";
-        } else if (author.toLowerCase().search("&%shaila%&") !== -1) {
-          rename.push({
-            "id": item.id,
-            "title": item.title.split("(")[0] + "( प्रप्रा. सौ शैला काळकर. )"
-          });
-          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( प्रप्रा. सौ शैला काळकर. )";
-        } else if (author.toLowerCase().search("&%rajan%&") !== -1) {
-          rename.push({
-            "id": item.id,
-            "title": item.title.split("(")[0] + "( सौ. रंजना जितेंद्र पाटील. )"
-          });
-          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( सौ. रंजना जितेंद्र पाटील. )";
-        } else if (author.toLowerCase().search("&%mona%&") !== -1) {
-          rename.push({
-            "id": item.id,
-            "title": item.title.split("(")[0] + "( श्रीमती मृदुला श्रीनिवास गडनीस. )"
-          });
-          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( श्रीमती मृदुला श्रीनिवास गडनीस. )";
-        } else if (author.toLowerCase().search("&%madhavi%&") !== -1) {
-          rename.push({
-            "id": item.id,
-            "title": item.title.split("(")[0] + "( डॉ. माधवी ठाकूरदेसाई. )"
-          });
-          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( डॉ. माधवी ठाकूरदेसाई. )";
-        } else if (author.toLowerCase().search("&%asavari%&") !== -1) {
-          rename.push({
-            "id": item.id,
-            "title": item.title.split("(")[0] + "( सौ आसावरी काळे. )"
-          });
-          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( सौ आसावरी काळे. )";
-        } else if (author.toLowerCase().search("&%manjiri%&") !== -1) {
-          rename.push({
-            "id": item.id,
-            "title": item.title.split("(")[0] + "( सौ मंजिरी फाटक. )"
-          });
-          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( सौ मंजिरी फाटक. )";
-        } else if (author.toLowerCase().search("&%kapil%&") !== -1) {
-          rename.push({
-            "id": item.id,
-            "title": item.title.split("(")[0] + "( श्री. कपिल देशपांडे. )"
-          });
-          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( श्री. कपिल देशपांडे. )";
-        } else if (author.toLowerCase().search("&%sandhya%&") !== -1) {
-          rename.push({
-            "id": item.id,
-            "title": item.title.split("(")[0] + "( श्रीमती संध्या देवस्थळे. )"
-          });
-          autorename = autorename + "✔️Renamed from " + item.title + " <====> " + item.title.split("(")[0] + "( श्रीमती संध्या देवस्थळे. )";
-        } else {
-          autorename = autorename + '<strong><H1 "style=color: red;">❌fatal error occured ' + author + " not found </H1><h6>title " + item.title + "<H6></strong>";
-          console.log(item.title);
-        }
-        console.log(rename)
-      }
-    }
-  });
-
 }
 
 var port = process.env.PORT || 3000;
