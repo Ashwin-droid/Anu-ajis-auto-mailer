02ca527cd625f0a4ec921687932696e5b02f8d3a
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 <ash6chin8@gmail.com>
2021-12-26 21:40:53 +0530

    test 2

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.get(`/autotrg/ifttt/auth/` + process.env.AUTH_KEY, (_req, res) => {
       htmlString += `<h6>Title:  <a href="https://www.buzzsprout.com/1173590/${websiteContent[i].id}">${websiteContent[i].title} </a>downloads: ${websiteContent[i].total_plays}</h6>`; 
     }
     htmlString += `<h1>Oldest 5</h1>`;
-    for (var i = 1; i == 5; i++){
+    for (var i = 1; i != 5; i++){
       htmlString += `<h6>Title:  <a href="https://www.buzzsprout.com/1173590/${websiteContent[websiteContent.length - i - 1].id}">${websiteContent [websiteContent.length - i - 1].title} </a>downloads: ${websiteContent[websiteContent.length - i - 1].total_plays}</h6>`; 
     }
     var preString = `<table><tr><td><h3>Author</h3></td><td><h3>Views</h3></td><td style='padding:10px'><h3>Entries</h3></td></tr>`;
