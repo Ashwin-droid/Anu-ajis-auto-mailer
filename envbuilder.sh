2f224acdd90705dc260c42fab4303d1acab86b1d
Ashwin-droid <58349846+Ashwin-droid@users.noreply.github.com>
2022-08-30 06:29:44 +0000

    debuging +++

--- envbuilder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-touch .env
-echo API_KEY=${{ secrets.API_KEY }} >> .env
-echo CLIENT_ID=${{ secrets.CLIENT_ID }} >> .env
-echo CLIENT_SECRET=${{ secrets.CLIENT_SECRET }} >> .env
-echo EMAIL=${{ secrets.EMAIL }} >> .env
-echo REFRESH_TOKEN=${{ secrets.REFRESH_TOKEN }} >> .env
-echo TARGET_MAIL_ID=${{ secrets.TARGET_MAIL_ID }} >> .env
-echo WRITE_ACC=${{ secrets.WRITE_ACC }} >> .env
